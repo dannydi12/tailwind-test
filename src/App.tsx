34632884991521b373185d81820f44660d0643b1
Daniel Di Venere <danieldivenere@gmail.com>
2023-01-24 17:34:57 -0800

    well, i guess I'm trying this...

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,34 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="mx-auto max-w-md overflow-hidden rounded-3xl bg-white shadow-md md:max-w-2xl">
+        <div className="md:flex">
+          <div className="md:shrink-0">
+            <img
+              className="h-48 w-full object-cover md:h-full md:w-48"
+              src="https://images.unsplash.com/photo-1674560109079-0b1cd708cc2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
+              alt="Modern building architecture"
+            />
+          </div>
+          <div className="p-8">
+            <div className="text-sm font-semibold uppercase tracking-wide text-indigo-500">
+              Company retreats
+            </div>
+            <a
+              href="#"
+              className="mt-1 block text-lg font-medium leading-tight text-black hover:underline"
+            >
+              Incredible accommodation for your team
+            </a>
+            <p className="mt-2 text-slate-500">
+              Looking to take your team away on a retreat to enjoy awesome food
+              and take in some sunshine? We have a list of places to do just
+              that.
+            </p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
